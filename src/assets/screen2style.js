07f357911e88da03db0ef202d9a0f13740e7e420
albseb511 <albert@wisense.in>
2019-07-27 14:09:11 +0530

    screen2

---
+++ src/assets/screen2style.js
@@ -0,0 +1,39 @@
+import {StyleSheet} from 'react-native'
+
+export default sc2 = StyleSheet.create({
+    sctionRow:{
+        flexDirection:'row',
+        flex:1,
+        height:60,
+        marginTop:50,
+        marginBottom:50,
+    },
+    text:{
+        flex:1,
+        //fontSize:20,
+        alignSelf:'center'
+    },
+    btn:{
+        height:50,
+        width:100,
+        backgroundColor:'blue',
+        borderRadius:10,
+        alignSelf:'center',
+        justifyContent:'center',
+        marginTop:40
+    },
+    btnText:{
+        color:'white',
+        fontSize:20,
+        alignSelf:'center',
+    },
+    input:{
+        //backgroundColor:'blue',
+        borderColor:'gray',
+        borderWidth:1,
+        borderRadius:10,
+        marginTop:50,
+    }
+  });
+  
+  
--- src/screens/screen2.js
+++ src/screens/screen2.js
@@ -1,20 +1,44 @@
 import React, {Component} from 'react'
-import {Text,View} from 'react-native'
+import {Text,View,
+        TextInput,
+        TouchableOpacity} from 'react-native'
 
 import styles from '../assets/style'
+import sc2 from '../assets/screen2style'
+
+//TODO
+// Stock value to be displayed, passed from navigation props
+// Once change val to number only input
+// Add/Edit button to push info after checking for valid response.
+// Airtable integration
 
 export default class Screen2 extends Component {
     constructor(props){
       super(props)
       this.state={
-        
-  
+        val:'',
+        stckVal:30
       }
     }
       render(){
           return(
                 <View style={styles.sectionContainer}>
-                    <Text style={styles.sectionTitle}>Screen 2</Text>
+                    <Text style={styles.sectionTitle}>Form</Text>
+
+                    <View style={sc2.sctionRow}>
+                        <Text style={sc2.text}>Stock Value</Text>
+                        <Text style={sc2.text}>{this.state.stckVal}</Text>
+                    </View>
+                    
+                    <Text style={styles.sectionTitle}>Change/Add Value</Text>
+                    <TextInput  style={sc2.input} 
+                                onChangeText={(val) => this.setState({val})}
+                                value={this.state.val}>
+                                    </TextInput>
+                    <TouchableOpacity style={sc2.btn}>
+                        <Text style={sc2.btnText}>Add/Edit</Text>
+                    </TouchableOpacity>
+
                 </View>
         
           )
